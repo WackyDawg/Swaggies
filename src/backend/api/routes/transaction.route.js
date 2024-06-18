f02ffe591a9f37c8b29cffec5655136055bd84cf
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-06-18 14:54:50 +0100

    Updated api

--- src/backend/api/routes/transaction.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import transactionController from '../controllers/transaction.controller.js';
-//import { auth } from "../middlewares/auth.js";
-
-const router = expressRouter();
-
-router.get('/transactions', [auth], transactionController.getTransactions)
-
-module.exports = router;
